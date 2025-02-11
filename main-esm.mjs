a59fb220a788d80f1b8d799e842e6af8e35a9a6f
Azharafridi <99399706+Azharafridi@users.noreply.github.com>
2025-02-11 19:34:12 +0500

    different import and export pattens

---
+++ main-esm.mjs
@@ -0,0 +1,12 @@
+import math from "./math-esm.mjs"
+
+// de-structure the function from the object.
+
+const {add, subtract} = math
+console.log(add(8, 2));
+console.log(subtract(8, 2));
+
+// this below is one the method above also works
+// console.log(math.add(8, 2));
+// console.log(math.subtract(8, 2));
+
---
+++ math-esm.mjs
@@ -0,0 +1,21 @@
+const add = (a, b) =>{
+    return a + b;
+};
+
+// export default add;
+
+const  subtract = (a, b) => {
+    return a - b
+}
+
+// this is third pattend as whole object is imported there
+export default {
+    add,
+    subtract,
+};
+
+// 2nd pattend
+
+// export default (a, b) => {
+//     return a + b;
+// };
